aa35b53c808a079a3b09df568c90a31e18237dff
mistekko <136386015+mistekko@users.noreply.github.com>
2024-07-14 08:21:12 +0000

    altered command that caused devcontainer building to hang
    'watchman version' causes the postCreateCommand.sh to hang. Switching it to 'watchman --version' fixes this.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,7 +6,7 @@ npm install --save-dev -y create-expo-app@2.1.1
 echo -e "\nInstalling watchman...\n"
 sudo apt update
 sudo apt install watchman
-watchman version
+watchman --version
 
 echo -e "\n*******************************"
 echo -e "\nDev container ready!".
